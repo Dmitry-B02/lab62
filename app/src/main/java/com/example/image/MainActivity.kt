8e061ef3d4c5241e2ba6ca587c2f1d3d97f5d7ca
Dmitry-B02 <silaxer@mail.ru>
2021-11-25 21:28:53 +0300

    1

---
+++ app/src/main/java/com/example/image/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.image
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.image.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import android.graphics.BitmapFactory
+import androidx.lifecycle.MutableLiveData
+import java.io.BufferedInputStream
+import java.net.URL
+
+
+class MainActivity: AppCompatActivity() {
+    private lateinit var executor: ExecutorService
+    private lateinit var binding: ActivityMainBinding
+    private val bitmapData = MutableLiveData<Bitmap>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (binding.image.drawable == null) {
+            loadImage()
+        }
+
+        bitmapData.observe(this) { value ->
+            if (value != null) {
+                binding.image.setImageBitmap(value)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        executor.shutdown()
+    }
+
+    private fun loadImage() {
+        executor = Executors.newFixedThreadPool(1)
+        executor.execute {
+            val newurl = URL("https://i.ibb.co/LCGjry8/Screenshot-20211121-231052-Gallery.jpg")
+            val stream = newurl.openConnection().getInputStream()
+            stream.use {
+                val mIcon_val = BitmapFactory.decodeStream(stream)
+                bitmapData.postValue(mIcon_val)
+            }
+        }
+    }
+}
