b37b6650cdc7cc27eb0783186f252e9ef679c1cf
Dmitry-B02 <silaxer@mail.ru>
2022-01-04 18:57:06 +0300

    1

--- app/src/main/java/com/example/image/MainActivity.kt
+++ app/src/main/java/com/example/image/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import com.example.image.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import android.graphics.BitmapFactory
+import android.view.View
+import android.widget.Button
+import androidx.core.view.isVisible
 import androidx.lifecycle.MutableLiveData
 import java.net.URL
 
@@ -14,20 +17,26 @@ class MainActivity: AppCompatActivity() {
     private lateinit var executor: ExecutorService
     private lateinit var binding: ActivityMainBinding
     private val bitmapData = MutableLiveData<Bitmap>()
+    private lateinit var download: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        download = findViewById(R.id.download)
 
-        if (binding.image.drawable == null) {
-            loadImage()
-        }
+        download.setOnClickListener {
 
-        bitmapData.observe(this) { value ->
-            if (value != null) {
-                binding.image.setImageBitmap(value)
+            if (binding.image.drawable == null) {
+                loadImage()
             }
+
+            bitmapData.observe(this) { value ->
+                if (value != null) {
+                    binding.image.setImageBitmap(value)
+                }
+            }
+            download.visibility = View.GONE
         }
     }
 
